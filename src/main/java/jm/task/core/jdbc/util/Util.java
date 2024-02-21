4418cb6d7fd04033c289ea6d71dd476ae707c730
Vadim <kach.domin@mail.ru>
2024-02-21 21:35:55 +0300

    MySQL db

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,23 +1,18 @@
 package jm.task.core.jdbc.util;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    private static final String URL = "jdbc:postgresql://localhost:5432/users_db";
-    private static final String NAME = "postgres";
-    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:mysql://localhost:3306/users_db";
+    private static final String NAME = "root";
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() {
         Connection connection = null;
-
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
 
         try {
             connection = DriverManager.getConnection(URL, NAME, PASSWORD);
