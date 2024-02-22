b9c217af4c2ac496e3a3161b867070ed37d80394
Vadim <kach.domin@mail.ru>
2024-02-22 19:17:15 +0300

    MySQL db

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,9 +6,11 @@ public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
         UserServiceImpl userService = new UserServiceImpl();
+        userService.dropUsersTable();
         userService.createUsersTable();
         userService.saveUser("vadim", "asd", (byte) 20);
         userService.saveUser("asdvadim", "asd", (byte) 25);
+        userService.removeUserById(1);
         System.out.println(userService.getAllUsers());
     }
 }
